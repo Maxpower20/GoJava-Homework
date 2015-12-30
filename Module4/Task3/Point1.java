303d897f580a4a2ed44867fdb09a59763e988346
Maxpower20 <maxymshpack@gmail.com>
2015-12-30 15:48:22 +0200

    Class Point1 with its attributes

---
+++ Module4/Task3/Point1.java
@@ -0,0 +1,31 @@
+package Module4.Task1.Task3;
+
+/**
+ * Created by Максим on 30.12.2015.
+ */
+public class Point1 {
+    private double x1;
+    private double y1;
+
+    Point1(double x1, double y1){
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    public double getX1() {
+        return this.x1;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public double getY1() {
+        return this.y1;
+    }
+}
+
