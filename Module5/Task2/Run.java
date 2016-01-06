ccd81725928f7537facc48f04b19e7452a720596
Maksym Shpak <maxymshpack@gmail.com>
2016-01-06 15:28:09 +0200

    Update and rename Runner.java to Run.java

---
+++ Module5/Task2/Run.java
@@ -0,0 +1,15 @@
+public class Run {
+    public static void main(String[] args) {
+
+        int arr[] ={62, 84, 32, 5, 0, 14, 52, 82, 58, 71};
+
+        Sorting.bubbleSort(arr);
+
+        for(int numbers: arr){
+            System.out.println(numbers);
+        }
+
+
+    }
+
+}
