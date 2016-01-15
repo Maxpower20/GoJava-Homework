f4b04d612529d0a239e5cecf2a2c017e7b2c0d12
Maksym Shpak <maxymshpack@gmail.com>
2016-01-15 12:39:21 +0200

    Update Bouquet.java

--- Module3/Task2/Bouquet.java
+++ Module3/Task2/Bouquet.java
@@ -6,13 +6,8 @@ import java.util.ArrayList;
  * Created by Максим on 28.12.2015.
  */
 public class Bouquet {
-    public static void main(String[] args) {
-        ArrayList<Flower> filesList = new ArrayList<Flower>();
-
-        filesList.add(new Aster());
-        filesList.add(new Chamomile());
-        filesList.add(new Tulip());
-        filesList.add(new Rosebush());
-        
-    }
+    private String name;
+    private int quantityOfFlowers;
+    
+    private List<Flower> flowers;
 }
