5c6f3362d5186ab5b83fc0589fe82f75cde68746
Maksym Shpak <maxymshpack@gmail.com>
2016-01-17 14:40:06 +0200

    Update Run.java

--- Module5/Task1/Run.java
+++ Module5/Task1/Run.java
@@ -2,12 +2,13 @@ public class Run {
     public static void main(String[] args) {
         int[] intArray = {12, 2, 44, 45, 1, 101, 104, 2016, 99};
 
-        Sort.sort(intArray);
+        System.out.println(Sort.findMax(intArray));
 
-        int min = intArray[0];
-        int max = intArray[intArray.length-1];
-        System.out.println("The biggest number in array is " + max);
-        System.out.println("The smallest number in array is " + min);
+        System.out.println(Sort.findMin(intArray));
+
 
     }
+        
+
+    
 }
