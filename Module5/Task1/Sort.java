b8a36af6bd9b7a2a75de107f1ddf00d46fe5bd49
Maksym Shpak <maxymshpack@gmail.com>
2016-01-17 14:40:27 +0200

    Update Sort.java

--- Module5/Task1/Sort.java
+++ Module5/Task1/Sort.java
@@ -1,5 +1,27 @@
 public class Sort {
-    public static void sort(int[] arr){
-        Arrays.sort(arr);
-    }
+    public static int findMax(int[] arr)
+        {
+            int arr_max = arr[0];
+            for (int i = 0; i < arr.length; i++)
+            {
+                if(arr[i] > arr_max)
+                {
+                    arr_max = arr[i];
+                }
+            }
+            System.out.print("Maximum integer of the array is ");
+            return arr_max;
+        }
+
+        public static int findMin(int arr[])
+        {
+            int arr_min = arr[0];
+            for (int i = 0; i < arr.length; i++)
+            {
+                if(arr[i] < arr_min)
+                    arr_min = arr[i];
+            }
+            System.out.print("Minimum integer of the array is ");
+            return arr_min;
+        }
 }
