e4538860179fe05b27b2b31e1a0397cab31023ed
Maksym Shpak <maxymshpack@gmail.com>
2016-01-24 16:03:24 +0100

    Create Runner.java

---
+++ Module8/Runner.java
@@ -0,0 +1,14 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Runner {
+    public static void main(String[] args) {
+       Set<Flower> flowers = new TreeSet<>(new FlowerNameComparator());
+        flowers.add(new Aster("Norwean Aster"));
+        flowers.add(new Chamomile("Ukrainian Chamomile"));
+        flowers.add(new Tulip("French Tulip"));
+
+        
+    }
+}
